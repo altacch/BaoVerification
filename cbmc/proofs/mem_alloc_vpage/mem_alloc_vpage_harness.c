41edae17dcec4d69e13260155aa166184c42da4a
Alberto Tacchella <alberto.tacchella@unitn.it>
2025-10-17 14:25:32 +0200

    Update mem_alloc_vpage harness

--- cbmc/proofs/mem_alloc_vpage/mem_alloc_vpage_harness.c
+++ cbmc/proofs/mem_alloc_vpage/mem_alloc_vpage_harness.c
@@ -37,49 +37,9 @@ struct section* mem_find_sec(struct addr_space* as, vaddr_t va)
     return &hyp_secs[idx];
 }
 
-/*
-pte_t* pt_get_pte(struct page_table* pt, size_t lvl, vaddr_t va)
-{
-    size_t i;
-    __CPROVER_assume(i < ROOT_SIZE);
-    pte_t* pte = &(pt->root[i]);
-
-    for (size_t i = 0; i < lvl; i++) {
-        if (!pte_valid(pte)) {
-            return NULL;
-        }
-        pte = pte_addr(pte);
-        size_t index;
-        __CPROVER_assume(index < ROOT_SIZE);
-        pte = &pte[index];
-    }
-    return pte;
-}
-*/
-
-pte_t* pt_get_pte(struct page_table* pt, size_t lvl, vaddr_t va)
-{
-    size_t pte_index = pt_getpteindex_by_va(pt, va, 0);
-    pte_t* pte = &(pt->root[pte_index]);
-
-    for (size_t i = 0; i < lvl; i++) {
-        if (!pte_valid(pte)) {
-            return NULL;
-        }
-        pte = pte_addr(pte);
-        size_t index = pt_getpteindex_by_va(pt, va, i+1);
-        pte = &pte[index];
-    }
-    return pte;
-}
-
-pte_t* pt_get(struct page_table* pt, size_t lvl, vaddr_t va)
-{
-    return pt_get_pte(pt, lvl, va);
-}
-
 pte_t* mem_alloc_pt(struct addr_space* as, pte_t* parent, size_t lvl, vaddr_t addr)
 {
+    /* to be improved */
     size_t i;
     __CPROVER_assume(i < ROOT_SIZE);    
     return &as->pt.root[i];
@@ -94,8 +54,9 @@ void harness(void)
 {
     struct addr_space* as;
     as = proof_as_allocate();
-    proof_hier_as_init(as);
-    assert(proof_hier_as_is_valid(as));
+    proof_as_init(as);
+    //    proof_pt_fill(&as->pt, 4);
+    assert(proof_as_is_valid(as));
 
     // we only deal with the hypervisor address space for now
     enum AS_SEC section;
@@ -103,19 +64,20 @@ void harness(void)
                      section == SEC_HYP_IMAGE ||
                      section == SEC_HYP_PRIVATE ||
                      section == SEC_HYP_VM);
-    /*    
+
     {
         /* Case 1: vaddr == INVALID_VA
-           allocate at first free block of section //
+           allocate at first free block of section */
         vaddr_t vaddr = INVALID_VA;
         size_t n;
-        __CPROVER_assume(n < MAX_N);
+        //__CPROVER_assume(n != 0);
+        __CPROVER_assume(n <= MAX_N);
         vaddr_t res;
 
         res = mem_alloc_vpage(as, section, vaddr, n);
         //assert();
     }
-*/
+
     {
         /* Case 2: vaddr != INVALID_VA
            allocate at specified address */
@@ -124,10 +86,12 @@ void harness(void)
         __CPROVER_assume(proof_vaddr_is_valid(vaddr));
         __CPROVER_assume(vaddr != INVALID_VA);
         size_t n;
-        __CPROVER_assume(n < MAX_N);
+        //__CPROVER_assume(n != 0);
+        __CPROVER_assume(n <= MAX_N);
         vaddr_t res;
 
         res = mem_alloc_vpage(as, section, vaddr, n);
         assert(res == INVALID_VA || proof_vaddr_is_valid(res));
     }
+
 }
